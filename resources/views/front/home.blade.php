1d47b49efe11fdaaf316414c7d03af1e088e750e
mahmoud <mahmoudghareeb11111@gmail.com>
2022-12-14 11:07:12 +0200

    adding search to emojis

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -152,12 +152,12 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
                         
                         `;
                     }
-
+                    
                     $("#put-search-data").html(html);
                     $(".emmo-select").on('click', function() {
                         $(this).toggleClass('fa-solid');
                         $(this).toggleClass('fa-regular');
-                        var ids = $("#feel_id").val();
+                        var ids = "";
                         $(".emmo-select").each(function() {
                             var clas = $(this).attr('class');
                             var dataid = $(this).attr('data-id');
